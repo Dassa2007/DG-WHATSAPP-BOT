eee2d947244a5d6dcd07d462e10d83694d82fcd0
PROGRAMMER_DASSA <dasungimhana230@gmail.com>
2026-04-05 22:00:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || '',
+    CREDS_JSON: process.env.CREDS_JSON || '',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://Dasun2007:bWDAWLiTXQZscbRE@dasun2.mutdzzm.mongodb.net/',
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/If2IrHuqGzTDyMD9HuTMRt?mode=gi_t',
+    ADMIN_LIST_PATH: './admin.json',
+    RCD_IMAGE_PATH: 'https://files.catbox.moe/z6bb14.jpg',
+    NEWSLETTER_JID: '120363421928318889@newsletter',
+    NEWSLETTER_MESSAGE_ID: '428',
+    OTP_EXPIRY: 300000,
+    NEWS_JSON_URL: '',
+    BOT_NAME: 'DG WHATSAPP BOT',
+    OWNER_NAME: 'DASUN GIMHANA',
+
+    OWNER_NUMBER: '94783188906',
+
+    BOT_VERSION: '1.0.0',
+    BOT_FOOTER: '> Developed By Dg Programmer',
+    CHANNEL_LINK: https:'//whatsapp.com/channel/0029VbD2xLiEAKWDIPHxYq3y',
+};
